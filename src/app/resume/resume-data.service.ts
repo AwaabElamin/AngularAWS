db0ee05bf9f455f9988634d2f2aa10bc28c26ff0
Awaab Elamin <AwaabElamin@gmail.com>
2023-07-31 13:25:33 -0500

    return back local back end for dev

--- src/app/resume/resume-data.service.ts
+++ src/app/resume/resume-data.service.ts
@@ -7,8 +7,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ResumeDataService{
-  private backendUrl: string = 'https://epdqkqaj3f.us-east-1.awsapprunner.com/resume';
-  // private backendUrl: string = 'http://localhost:3000/resume';
+  // private backendUrl: string = 'https://epdqkqaj3f.us-east-1.awsapprunner.com/resume';
+  private backendUrl: string = 'http://localhost:3000/resume';
   private resume!: Observable<Resume>;
   // private headers= new HttpHeaders()
   // .set('Access-Control-Allow-Origin', '*')
