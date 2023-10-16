2d8d5a1dbd7febb52adac93099a63ec447f99ba3
Awaab Elamin <AwaabElamin@gmail.com>
2023-10-16 11:39:03 -0500

    update new user form

--- src/app/login/new-user/new-user.component.ts
+++ src/app/login/new-user/new-user.component.ts
@@ -14,7 +14,7 @@ export class NewUserComponent {
     firstname: new FormControl('', Validators.required),
     lastname: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
+    // username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
   });
   formStatus:string = this.newUserForm.status;
