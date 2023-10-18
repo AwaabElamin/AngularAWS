75f09a0daf8ff63b5392d855492c89af572cb571
Awaab Elamin <AwaabElamin@gmail.com>
2023-10-18 11:34:01 -0500

    download as pdf

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { ResumeDataService } from './resume-data.service';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-resume',
@@ -7,6 +9,22 @@ import { ResumeDataService } from './resume-data.service';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
-  title:string = "Awaab Elamin";
-  
+  title: string = "Awaab Elamin";
+  public SavePDF(): void {
+    var data: any = document.getElementById("content");
+    console.log(data)
+    html2canvas(data)
+      .then(canvas => {
+        var imgWidth = 200;
+        var pageHeight = 295;
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        const contentDataURL = canvas.toDataURL('image/png');
+        let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+        var position = 5;
+        pdf.addImage(contentDataURL, 'PNG', 5, position, imgWidth, imgHeight)
+        pdf.save('new-file.pdf'); // Generated PDF
+      }
+      );
+  }
 }
