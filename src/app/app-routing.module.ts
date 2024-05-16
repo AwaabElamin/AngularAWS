d1e64efefd7536f6b0d7a6a2e2f762604849b483
Awaab Elamin <AwaabElamin@gmail.com>
2024-05-15 23:24:19 -0500

    add project routing to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from './auth-guard.guard';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) },
   { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: 'bbc', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }
+  { path: 'project',canActivateChild:[AuthGuardGuard], loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }
 ];
 
 @NgModule({
