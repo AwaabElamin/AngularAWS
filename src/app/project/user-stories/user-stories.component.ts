d7b73ef6559fbafd1d08cf4bb985bf2134e70a0b
Awaab Elamin <AwaabElamin@gmail.com>
2024-05-15 23:23:10 -0500

    add user-stories component to project

--- src/app/project/user-stories/user-stories.component.ts
+++ src/app/project/user-stories/user-stories.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-stories.component.css']
 })
 export class UserStoriesComponent {
-
+  actors = [
+    "Customer",
+    "Admin",
+    "Manager",
+    "HR",
+    "Employee"
+  ];
+  CRUDs = [
+    "Create",
+    "Read",
+    "Update",
+    "Delete"
+  ]
+  selectedActor = "----";
+  selectedCRUD = "----";
+  InputActionValue = "----";
+  updateActor(e: any) {
+    this.selectedActor = e.target.value;
+  }
+  updateCRUD(e: any) {
+    this.selectedCRUD = e.target.value;
+  }
+  updateInputAction(e: any) {
+    this.InputActionValue = e.target.value;
+  }
 }
