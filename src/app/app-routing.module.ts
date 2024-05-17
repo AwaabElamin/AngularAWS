e9bd2d59b30c654e83218a6b078f0994f33f9f84
Awaab Elamin <AwaabElamin@gmail.com>
2024-05-16 19:20:33 -0500

    add authorization for resume and user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { AuthGuardGuard } from './auth-guard.guard';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'resume',canActivateChild:[AuthGuardGuard], loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) },
+  { path: 'user',canActivateChild:[AuthGuardGuard], loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
   { path: 'project',canActivateChild:[AuthGuardGuard], loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }
 ];
 
