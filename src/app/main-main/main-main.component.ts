edf71858fd278d6f5426a59aa4c0687dbaffe586
Awaab Elamin <AwaabElamin@gmail.com>
2024-08-12 12:39:32 -0500

    get data of summary from component

--- src/app/main-main/main-main.component.ts
+++ src/app/main-main/main-main.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-main',
   templateUrl: './main-main.component.html',
   styleUrls: ['./main-main.component.css']
 })
-export class MainMainComponent {
-
+export class MainMainComponent implements OnInit{
+  summary: string = "";
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    this.summary = "Results-driven Full Stack Developer with 5 years of hands-on experience in designing and implementing"
+        + "innovative web applications. Adept at utilizing a comprehensive skill set, with a particular emphasis"
+        + "on JavaScript technologies. Proven track record of delivering high-quality solutions through the entire"
+        + "software development lifecycle. Excels in collaborating with cross- functional teams to drive project success. A"
+        + "detail-oriented problem solver with a passion for staying abreast of emerging technologies and industry best"
+        + "practices. Recognized for consistently meeting project deadlines and exceeding client expectations. Seeking to"
+        + "contribute expertise and enthusiasm to a dynamic development team."
+  }
 }
