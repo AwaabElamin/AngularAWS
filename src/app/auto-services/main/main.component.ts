30f57b5554fafaf0937761b8c8db1ac58bde0a50
AwaabElamin <AwaabElamin@gmail.com>
2024-12-19 13:25:52 -0600

    update auto compo

--- src/app/auto-services/main/main.component.ts
+++ src/app/auto-services/main/main.component.ts
@@ -1,15 +1,23 @@
-import { Component, Sanitizer } from '@angular/core';
+import { Component, OnInit, Sanitizer } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { AutoService } from '../auto.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  user:{} | undefined;
   urlIframe:any =  this.sanitizer.bypassSecurityTrustResourceUrl("https://main.dfnradbwueq86.amplifyapp.com/usedcars/new");
-  constructor(private router:Router, private sanitizer: DomSanitizer){}
+  constructor(private router:Router, private sanitizer: DomSanitizer, private autoService:AutoService){}
+  ngOnInit(){
+    this.autoService.getCurrentAutoUser()
+    .subscribe(data=>{
+      console.log('user at auto hub', data);
+    });
+  }
   btnNewClicked(){
     this.urlIframe = this.sanitizer.bypassSecurityTrustResourceUrl("https://main.dfnradbwueq86.amplifyapp.com/usedcars/new");
     console.log("iframe url", this.urlIframe);
